0341f5f5bfa7a126af0edd93cb10311c1e2af467
daxin007 <1426089918@qq.com>
2023-07-21 10:16:56 +0800

    update_code

---
+++ models/encoder.py
@@ -0,0 +1,109 @@
+import math
+from typing import List
+
+import torch
+from torch import nn
+import torch.nn.functional as F
+import torch.fft as fft
+from einops import reduce, rearrange, repeat
+
+import numpy as np
+
+class BandedFourierLayer(nn.Module):
+    def __init__(self, in_channels, out_channels, band, num_bands, length=201):
+        super().__init__()
+
+        self.length = length
+        self.total_freqs = (self.length // 2) + 1
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+
+        self.band = band  # zero indexed
+        self.num_bands = num_bands
+
+        self.num_freqs = self.total_freqs // self.num_bands + (self.total_freqs % self.num_bands if self.band == self.num_bands - 1 else 0)
+
+        self.start = self.band * (self.total_freqs // self.num_bands)
+        self.end = self.start + self.num_freqs
+
+
+        # case: from other frequencies
+        self.weight = nn.Parameter(torch.empty((self.num_freqs, in_channels, out_channels), dtype=torch.cfloat))
+        self.bias = nn.Parameter(torch.empty((self.num_freqs, out_channels), dtype=torch.cfloat))
+        self.reset_parameters()
+
+    def forward(self, input):
+        # input - b t d
+        b, t, _ = input.shape
+        input_fft = fft.rfft(input, dim=1)
+        output_fft = torch.zeros(b, t // 2 + 1, self.out_channels, device=input.device, dtype=torch.cfloat)
+        output_fft[:, self.start:self.end] = self._forward(input_fft)
+        return fft.irfft(output_fft, n=input.size(1), dim=1)
+
+    def _forward(self, input):
+        output = torch.einsum('bti,tio->bto', input[:, self.start:self.end], self.weight)
+        return output + self.bias
+
+    def reset_parameters(self) -> None:
+        nn.init.kaiming_uniform_(self.weight, a=math.sqrt(5))
+        fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.weight)
+        bound = 1 / math.sqrt(fan_in) if fan_in > 0 else 0
+        nn.init.uniform_(self.bias, -bound, bound)
+
+
+class CoSTEncoder(nn.Module):
+    def __init__(self, input_dims, output_dims,
+                 kernels: List[int],
+                 length: int,
+                 hidden_dims=64, depth=10,
+                 mask_mode='binomial'):
+        super().__init__()
+
+        component_dims = output_dims // 2
+        #component_dims = output_dims
+
+        self.input_dims = input_dims
+        self.output_dims = output_dims
+        self.component_dims = component_dims
+        self.input_fc = nn.Linear(input_dims, output_dims)
+
+        self.kernels = kernels
+
+        self.tfd = nn.ModuleList(
+            [nn.Conv1d(component_dims, component_dims, k, padding=k-1) for k in kernels]
+        )
+
+        self.sfd = nn.ModuleList(
+            [BandedFourierLayer(component_dims, component_dims, b, 1, length=length) for b in range(1)]
+        )
+
+    def forward(self, x, tcn_output=False, mask='all_true'):  # x: B x T x input_dims
+        nan_mask = ~x.isnan().any(axis=-1)
+        x[~nan_mask] = 0
+        x = self.input_fc(x)  # B x T x Ch        
+
+        d = int(x.shape[-1]/2)
+        x_s = x[:,:,:d] #season
+        x_t = x[:,:,d:] #trend
+        x_t = x_t.transpose(1, 2)  # B x Ch x T
+
+        trend = []
+        for idx, mod in enumerate(self.tfd):
+            out = mod(x_t)  # b d t
+            if self.kernels[idx] != 1:
+                out = out[..., :-(self.kernels[idx] - 1)]
+            trend.append(out.transpose(1, 2))  # b t d
+        trend = reduce(
+            rearrange(trend, 'list b t d -> list b t d'),
+            'list b t d -> b t d', 'mean'
+        )
+
+        season = []
+        for mod in self.sfd:
+            out = mod(x_s)  # b t d
+            season.append(out)
+        season = season[0]
+        result = torch.cat([season,trend],dim=-1)
+
+        return result
